82b025466ce89f7b2e75504ea136bbbd9539c340
cordelia-sixth <80014084+cordelia-sixth@users.noreply.github.com>
2025-03-09 22:07:05 +0900

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '/react-vite-github-pages-demo/'
-  }
+  base: '/react-vite-github-pages-demo/'
 })
